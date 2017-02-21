cfb41ea66bb490ba85d983eb320a1abbd7d33ced
Maxim Gomon <flymandude@gmail.com>
2017-02-21 21:08:42 +0200

    Add some feature to StartWithReflection project

--- StartWithReflection/StartWithReflection/Program.cs
+++ StartWithReflection/StartWithReflection/Program.cs
@@ -12,7 +12,7 @@ namespace StartWithReflection
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
             //GetInfoAboutCurrentAssembly();
-            GetInfoAboutAssembly(@"D:\Work\Artinov\StartWithReflection\Warehouse\bin\Debug\warehouse.dll");
+            GetInfoAboutAssembly(@"D:\Work\Artinov\StartWithReflection\StartWithReflection\bin\Debug\StartWithReflection.exe");
             Console.ReadLine();
         }
 
@@ -26,8 +26,64 @@ namespace StartWithReflection
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine(definedType.Name);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                var item = someAssembly.CreateInstance(definedType.FullName);
+                Console.WriteLine("Properties:");
+                Console.WriteLine(new string('-', 30));
                 Console.ForegroundColor = ConsoleColor.Cyan;
+
+                //get all properties of type
+                foreach (var propertyInfo in definedType.DeclaredProperties)
+                {
+                    //create object array for property parameters
+                    object[] array = new object[1];
+                    //if type of current property is int => write int to object array
+                    if (propertyInfo.PropertyType == typeof (int))
+                    {
+                        array[0] = 1;
+                    }
+                    //if type of current property is string => write string to object array
+                    else if (propertyInfo.PropertyType == typeof(string))
+                    {
+                        array[0] = "Some text";
+                    }
+                    //invoke property setter
+                    propertyInfo.SetMethod.Invoke(item, array);
+                    string propertyDescription = String.Empty;
+                    propertyDescription += propertyInfo.PropertyType.Name;
+                    propertyDescription += " " + propertyInfo.Name + " {";
+                    if (propertyInfo.GetMethod != null)
+                    {
+                        propertyDescription += "get;";
+                    }
+                    if (propertyInfo.SetMethod != null)
+                    {
+                        propertyDescription += "set;";
+                    }
+                    propertyDescription += "}";
+                    Console.WriteLine(propertyDescription);
+                }
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+                Console.WriteLine("Fields:");
+                Console.WriteLine(new string('-', 30));
+                Console.ForegroundColor = ConsoleColor.Cyan;
+
+                //get all properties of type
+                foreach (var propertyInfo in definedType.DeclaredFields)
+                {
+                    string fieldDescription = String.Empty;
+                    fieldDescription += propertyInfo.FieldType.Name + " " + propertyInfo.Name;
+                    Console.WriteLine(fieldDescription);
+                }
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
                 Console.WriteLine("Methods:");
+                Console.WriteLine(new string('-', 30));
+                Console.ForegroundColor = ConsoleColor.Cyan;
+
                 List<MethodInfo> methods = definedType.DeclaredMethods
                     .Where(x => !x.IsSpecialName).ToList();
                 if (methods.Count == 0)
@@ -38,9 +94,20 @@ namespace StartWithReflection
                 }
                 else
                 {
+                    //Get all methods of current type
                     foreach (var methodInfo in methods)
                     {
                         Console.WriteLine(CreateMethodDeclarationString(methodInfo));
+                        if (methodInfo.ReturnType != typeof (void))
+                        {
+                            //invoke not void method
+                            Console.WriteLine(methodInfo.Invoke(item, null));
+                        }
+                        else
+                        {
+                            //invoke void method
+                            methodInfo.Invoke(item, null);
+                        }
                     }
                 }
             }
